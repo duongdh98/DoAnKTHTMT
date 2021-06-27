02f049a5858eaecce5da705fd5cb41f35aa5f58b
DuongDuongKLC <sakeklc9988@gmail.com>
2021-06-27 21:01:28 +0700

    moccauhon

--- public/js/evt.js
+++ public/js/evt.js
@@ -4,6 +4,9 @@ var conutl1 = 0 ;
 var conutl2 = 0 ;
 var conutl3 = 0 ;
 
+var https = "http://";
+var id = "192.168.1.40";
+
 function led1_on(){
     $(".pic_light1").css("fill","white");
 }
@@ -29,10 +32,22 @@ $( document ).ready(function() {
 
     $( "#eq1" ).click(function() {
         conutl1 ++;
+
         if(parseInt(conutl1) == 1){
+            var st = "/led1-on";
+            var url = https + id + st;
+
+            $(this).load( url, function(){
+                $(".leds1 .status").text("ON")
+            });
             led1_on();
         }
         if(parseInt(conutl1) == 2){
+            var st = "/led1-off";
+            var url = https + id + st;
+            $(this).load( url, function(){
+                $(".leds1 .status").text("OFF")
+            });
             led1_off();
             conutl1 = 0;
         }
@@ -40,9 +55,22 @@ $( document ).ready(function() {
     $( "#eq2" ).click(function() {
         conutl2 ++;
         if(parseInt(conutl2) == 1){
+            var st = "/led2-on";
+            var url = https + id + st;
+
+            $(this).load( url, function(){
+                $(".leds2 .status").text("ON")
+            });
             led2_on();
         }
         if(parseInt(conutl2) == 2){
+
+            var st = "/led2-off";
+            var url = https + id + st;
+
+            $(this).load( url, function(){
+                $(".leds2 .status").text("OFF")
+            });
             led2_off();
             conutl2 = 0;
         }
@@ -50,9 +78,21 @@ $( document ).ready(function() {
     $( "#eq3" ).click(function() {
         conutl3 ++;
         if(parseInt(conutl3) == 1){
+            var st = "/led3-on";
+            var url = https + id + st;
+
+            $(this).load( url, function(){
+                $(".leds3 .status").text("ON")
+            });
             led3_on();
         }
         if(parseInt(conutl3) == 2){
+            var st = "/led3-off";
+            var url = https + id + st;
+
+            $(this).load( url, function(){
+                $(".leds3 .status").text("OFF")
+            });
             led3_off();
             conutl3 = 0;
         }
